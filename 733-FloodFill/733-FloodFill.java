9fa5c40303a23512aeed0361942de1dceca710db
Madhan-kumar-E <madhanmischiefmm@gmail.com>
2025-06-27 14:30:24 +0530

    File updated at 6/27/2025, 2:18:54 PM

--- 733-FloodFill/733-FloodFill.java
+++ 733-FloodFill/733-FloodFill.java
@@ -1,4 +1,4 @@
-// Last updated: 6/27/2025, 1:50:57 PM
+// Last updated: 6/27/2025, 2:18:54 PM
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int temp=image[sr][sc];
@@ -6,19 +6,15 @@ class Solution {
         int m=image.length;
         int n=image[0].length;
         if (temp == color) return image; 
-        // Check right
         if(sc+1<n && image[sr][sc+1]==temp){
            floodFill(image,sr,sc+1,color);
         }
-        // Check left
         if(sc-1>=0 && image[sr][sc-1]==temp){
            floodFill(image,sr,sc-1,color);
         }
-        // Check up
         if(sr-1>=0 && image[sr-1][sc]==temp){
            floodFill(image,sr-1,sc,color);
         }
-        // Check down
         if(sr+1<m && image[sr+1][sc]==temp){
            floodFill(image,sr+1,sc,color);
         }
