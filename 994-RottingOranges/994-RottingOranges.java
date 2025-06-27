4c3ba25e1d1d26a01d3aee91fe3ace6336366041
Madhan-kumar-E <madhanmischiefmm@gmail.com>
2025-06-27 14:01:16 +0530

    File updated at 6/27/2025, 1:49:46 PM

--- 994-RottingOranges/994-RottingOranges.java
+++ 994-RottingOranges/994-RottingOranges.java
@@ -1,24 +1,37 @@
-// Last updated: 6/27/2025, 1:48:52 PM
+// Last updated: 6/27/2025, 1:49:46 PM
 class Solution {
-    public int islandPerimeter(int[][] grid) {
-        int perimeter = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == 1) {
-                    perimeter += 4;
-                    if (r > 0 && grid[r-1][c] == 1) {
-                        perimeter -= 2;
-                    }
-                    if (c > 0 && grid[r][c-1] == 1) {
-                        perimeter -= 2;
-                    }
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] vis = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
+        int time = 0, freshOranges = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 2){
+                    q.add(new int[]{i,j,0});
+                    vis[i][j] = 1;
+                }
+                if(grid[i][j] == 1) freshOranges++;
+            }
+        }
+        int oranges = 0;
+        int[] dRow = {-1,0,1,0};
+        int[] dCol = {0,1,0,-1};
+        while(!q.isEmpty()){
+            int[] rotten = q.poll();
+            int row = rotten[0], col = rotten[1], t = rotten[2];
+            time = Math.max(time,t);
+            for(int i = 0; i<4; i++){
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
+                if((nRow<n && nRow>=0) && (nCol<m && nCol>=0) && (vis[nRow][nCol] == 0 && grid[nRow][nCol] == 1)){
+                    q.add(new int[]{nRow,nCol,t+1});
+                    vis[nRow][nCol] = 1;
+                    oranges++;
                 }
             }
         }
-        
-        return perimeter;
+        return oranges==freshOranges?time:-1;
     }
 }
