5a98bca32c1ff12a3280511c3274902ebc60c8f4
Madhan-kumar-E <madhanmischiefmm@gmail.com>
2025-06-27 16:38:38 +0530

    Create 547-NumberofProvinces

---
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -0,0 +1,26 @@
+// Last updated: 6/27/2025, 4:27:08 PM
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int ans = 0;
+        boolean visited[] = new boolean[isConnected.length];
+
+        for(int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                depthFirstSearch(i,visited,isConnected);
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    void depthFirstSearch(int i,boolean visited[],int[][] isConnected) {
+        visited[i] = true;
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j]  == 1 && !visited[j]){
+                depthFirstSearch(j,visited,isConnected);
+            }
+        }
+    }
+
+}
