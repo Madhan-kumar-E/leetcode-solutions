05f5368b3488a6034d92604f7c568eccddf5bc72
Madhan-kumar-E <madhanmischiefmm@gmail.com>
2025-06-27 12:08:26 +0530

    Create 203-RemoveLinkedListElements

---
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
@@ -0,0 +1,16 @@
+// Last updated: 6/27/2025, 11:56:56 AM
+class Solution {
+  public ListNode removeElements(ListNode head, int val) {
+    ListNode dummy = new ListNode(0, head);
+    ListNode prev = dummy;
+
+    for (; head != null; head = head.next)
+      if (head.val != val) {
+        prev.next = head;
+        prev = prev.next;
+      }
+    prev.next = null; // In case that the last value equals `val`.
+
+    return dummy.next;
+  }
+}
